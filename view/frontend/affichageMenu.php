6fd7158c4e01a09664d1e742d4571bda8a05a646
Allan Boite <allan.boite@gmail.com>
2020-02-28 12:11:56 +0100

    Finalisation du menu avec la partie admin et la partie visiteur

--- view/frontend/affichageMenu.php
+++ view/frontend/affichageMenu.php
@@ -35,39 +35,29 @@
               <li class="nav-item">
                   <a class="nav-link" href="index.php?action=pageContact">Contact</a>
               </li>
+              
+              <!-- Si une session est ouverte alors on affiche le menu d'un utilisateur -->
+              <?php if(isset($_SESSION['id'])) { ?>
+            
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil</a>
 
-              <li class="nav-item">
-                  <a class="nav-link" href="index.php?action=pageInscription">Inscription</a>
-             </li>
-
-             <li class="nav-item">
-                  <a class="nav-link" href="index.php?action=pageConnexion">Connexion</a>
-              </li>
-                              <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil</a>
-                  
-                  <!-- bloc menu déroulant -->
+              <!-- bloc menu déroulant -->
                   <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                       <a class="dropdown-item" href="index.php?action=pageGestionProfil">Gestion profil</a>
                       <a class="dropdown-item" href="index.php?action=pageDeconnexion">Deconnexion</a>
                   </div>
                 </li>
-              <!-- Si une session est ouverte alors on affiche le menu d'un utilisateur -->
-              <!--<?php if(isset($_SESSION['id'])) { ?>
-            
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil</a> -->
-
             
               <!-- Si une session est ouverte et si c'est un admin on affiche le menu admin -->   
-              <!--<?php if (isset($_SESSION['id']) && ($_SESSION['admin'])){ ?>
+              <?php if (isset($_SESSION['id']) && ($_SESSION['admin'])){ ?>
 
                 <li class="nav-item">
                   <a class="nav-link" href="index.php?action=pageAdmin"><b>Gestion admin</b></a>
-                </li> -->
+                </li> 
      
             <!-- Sinon on affiche le menu de base -->    
-            <!--<?php } }else { ?>
+            <?php } }else { ?>
 
               <li class="nav-item">
                   <a class="nav-link" href="index.php?action=pageInscription">Inscription</a>
@@ -77,7 +67,7 @@
                   <a class="nav-link" href="index.php?action=pageConnexion">Connexion</a>
               </li>
 
-            <?php } ?>  -->
+            <?php } ?>  
 
           </ul>
       </div>
