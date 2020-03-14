9b8cfd71ff0261dcbf25685baf0c78d75ba20033
Allan Boite <allan.boite@gmail.com>
2020-03-15 00:33:10 +0100

    Amélioration du code dans plusieurs fichiers

--- view/backend/affichageMenu.php
+++ view/backend/affichageMenu.php
@@ -47,7 +47,7 @@
               <?php if (isset($_SESSION['id']) && ($_SESSION['admin'])){ ?>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="index.php?action=pageAdmin"><b>Gestion admin</b></a>
+                  <a class="nav-link" href="index.php?action=pageAdmin"><b>Gestion Administrateur</b></a>
                 </li> 
      
             <!-- Sinon on affiche le menu de base -->    
